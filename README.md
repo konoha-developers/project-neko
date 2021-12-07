88097a367edf2d033790fb304d3e84ef9a97685c
Bhavesh <bhic2030@gmail.com>
2021-12-07 12:05:50 +0530

    base project complete

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # project-neko
 First Project at Konoha (Work Under Progress)
+
+
+### Cat Api's
+
+Cat Facts Api - https://alexwohlbruck.github.io/cat-facts/
+
+Cataas - https://cataas.com/
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
-console.log("js loaded")
+const catContainer = document.getElementById("cat-container");
+const catFacts = document.getElementById("cat-facts");
+const loadMoreButton = document.getElementById("load-more-button");
+
+const fetchCatFacts = () => {
+  loadMoreButton.innerText = "Loading...";
+
+  fetch("https://cat-fact.herokuapp.com/facts/random?amount=3")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      data.forEach((fact) => {
+        catFacts.innerHTML += `
+        <div  class="fact-block">
+            <p>${fact.text}</p>
+        </div>
+            `;
+      });
+
+      loadMoreButton.innerText = "Load More";
+    });
+};
+
+const fetchCatImage = () => {
+  const loadingText = `<h1>Loading...</h1>`;
+  catContainer.innerHTML = loadingText;
+
+  fetch("https://cataas.com/cat?json=true")
+    .then((res) => res.json())
+    .then((data) => {
+      const result = `<img id="cat-image" src="https://cataas.com/${data.url}">`;
+      console.log(data);
+
+      catContainer.innerHTML = result;
+    });
+};
+
+fetchCatFacts();
+fetchCatImage();
